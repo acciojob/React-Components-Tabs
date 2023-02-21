c24d2672f63d1d9a898a459340da85edeed312f3
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-02-21 12:46:17 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,40 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import "../styles/App.css"
+import React, { useState } from 'react';
+import Tabs from "./Tabs"
 
+function App(){
+
+  // const tabNames = ["Home","About", "Features"]
+  const [tabNames,setTabNames] = useState(["Home","About", "Features"])
+
+    return (
+      <div className='app'>
+          <div className='browser'>
+            <Tabs tabNames={tabNames}/>
+            <div className="viewport">Pages Go Here</div>
+          </div>
+      </div>
+    );
+}
+
+// class App extends React.Component{
+
+//   constructor(props){
+//     super(props)
+//     this.state = {
+//       tabNames: ["Home","About", "Features"]
+//     }
+//   }
+//   render(){
+//     return (
+//       <div className='app'>
+//           <div className='browser'>
+//             <Tabs tabNames={this.state.tabNames}/>
+//             <div className="viewport">Pages Go Here</div>
+//           </div>
+//       </div>
+//     );
+//   }
+// }
+
+export default App;
